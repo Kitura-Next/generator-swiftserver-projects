8dcd1109b9ec233aa2d14c9f4fb1dcc21b8d7114
desertsurfer <72644721+desertsurfer@users.noreply.github.com>
2020-10-28 17:16:16 +0900

    revert to port 8080

--- Sources/Generator-Swiftserver-Projects/main.swift
+++ Sources/Generator-Swiftserver-Projects/main.swift
@@ -4,7 +4,7 @@ import Kitura
 let router = Router()
 
 // Add an HTTP server and connect it to the router
-Kitura.addHTTPServer(onPort: 8090, with: router)
+Kitura.addHTTPServer(onPort: 8080, with: router)
 
 // Start the Kitura runloop (this call never returns)
 Kitura.run()
