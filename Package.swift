a68b213846e9c95ba2455f9af4b3bca71a90ff7e
desertsurfer <desertsurfer@users.noreply.github.com>
2020-10-28 16:10:50 +0900

    Update to Kitura-Next

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@ import PackageDescription
 let package = Package(
     name: "Generator-Swiftserver-Projects",
     dependencies: [
-      .package(url: "https://github.com/IBM-Swift/Kitura.git", .upToNextMinor(from: "2.8.0")),
-      .package(url: "https://github.com/IBM-Swift/HeliumLogger.git", from: "1.7.1"),
-      .package(url: "https://github.com/IBM-Swift/CloudEnvironment.git", from: "9.0.0"),
+      .package(url: "https://github.com/Kitura-Next/Kitura.git", .upToNextMinor(from: "2.8.0")),
+      .package(url: "https://github.com/Kitura-Next/HeliumLogger.git", from: "1.7.1"),
+      .package(url: "https://github.com/Kitura-Next/CloudEnvironment.git", from: "9.0.0"),
       .package(url: "https://github.com/RuntimeTools/SwiftMetrics.git", from: "2.0.0"),
-      .package(url: "https://github.com/IBM-Swift/Kitura-OpenAPI.git", from: "1.0.0"),
-      .package(url: "https://github.com/IBM-Swift/Health.git", from: "1.0.0"),
+      .package(url: "https://github.com/Kitura-Next/Kitura-OpenAPI.git", from: "1.0.0"),
+      .package(url: "https://github.com/Kitura-Next/Health.git", from: "1.0.0"),
     ],
     targets: [
       .target(name: "Generator-Swiftserver-Projects", dependencies: [ .target(name: "Application") ]),
